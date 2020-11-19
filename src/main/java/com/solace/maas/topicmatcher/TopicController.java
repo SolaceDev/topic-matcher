6dd261450e0dbafea1997d3e140cfbe62df259fa
Michael Davis <michael@damaru.com>
2020-11-19 13:30:29 -0500

    Fixed REST controller.

--- src/main/java/com/solace/maas/topicmatcher/TopicController.java
+++ src/main/java/com/solace/maas/topicmatcher/TopicController.java
@@ -1,10 +1,10 @@
 package com.solace.maas.topicmatcher;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class RestController {
+@RestController
+public class TopicController {
 
     @GetMapping("/hello")
     public String hello() {
